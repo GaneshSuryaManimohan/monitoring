3f429fb527b064c05620b434869db18ed302ddaa
TGS_Manimohan <surya.manimohan@gmail.com>
2026-07-27 18:26:28 +0530

    updated

--- prometheus.sh
+++ prometheus.sh
@@ -14,9 +14,9 @@ TIME_STAMP=$(date +%F-%H-%M-%S)
 USERID=$(id -u)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
+
+PROMETHEUS_VERSION="3.13.1"
 PROMETHEUS_DIR="/opt/prometheus"
-
-
 
 if [ $USERID -ne 0 ]
 then
@@ -44,39 +44,82 @@ if [ -d "$PROMETHEUS_DIR" ]
 then
     echo -e "Prometheus already downloaded....$Y SKIPPING $N" | tee -a $LOGFILE
 else
-    wget https://github.com/prometheus/prometheus/releases/download/v3.13.1/prometheus-3.13.1.linux-amd64.tar.gz &>>$LOGFILE
+    wget -q https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz -O prometheus.tar.gz &>>$LOGFILE
     VALIDATE $? "Downloading Prometheus"
 
     tar -xf prometheus.tar.gz &>>$LOGFILE
     VALIDATE $? "Extracting Prometheus"
 
-    mv "prometheus-.linux-amd64" "$PROMETHEUS_DIR" &>>$LOGFILE
+    mv "prometheus-${PROMETHEUS_VERSION}.linux-amd64" "$PROMETHEUS_DIR" &>>$LOGFILE
     VALIDATE $? "Renaming Prometheus directory"
 
     rm -f prometheus.tar.gz &>>$LOGFILE
     VALIDATE $? "Cleaning up archive"
 fi
 
-cp -f /home/ec2-user/monitoring/prometheus.service /etc/systemd/system/prometheus.service
-VALIDATE $? "Copying Prometheus service file"
+# Track whether anything changed, so we only reload/restart when needed
+NEEDS_RESTART=0
+
+# Idempotent service file copy
+if ! cmp -s /home/ec2-user/monitoring/prometheus.service /etc/systemd/system/prometheus.service 2>/dev/null
+then
+    cp -f /home/ec2-user/monitoring/prometheus.service /etc/systemd/system/prometheus.service &>>$LOGFILE
+    VALIDATE $? "Copying Prometheus service file"
+    NEEDS_RESTART=1
+else
+    echo -e "Prometheus service file unchanged....$Y SKIPPING $N" | tee -a $LOGFILE
+fi
 
 mkdir -p /opt/prometheus/alert-rules &>>$LOGFILE
 VALIDATE $? "Creating alert-rules directory for Prometheus"
 
-cp -f /home/ec2-user/monitoring/*.yml /opt/prometheus/alert-rules &>>$LOGFILE
+# Sync alert rules; -u only copies newer/changed files, and we detect
+# whether anything actually changed for the restart decision
+CHANGED_RULES=$(cp -uv /home/ec2-user/monitoring/*.yml /opt/prometheus/alert-rules/ 2>>$LOGFILE | wc -l)
 VALIDATE $? "Copying alert rules files to Prometheus alert-rules directory"
+if [ "$CHANGED_RULES" -gt 0 ]
+then
+    NEEDS_RESTART=1
+fi
 
-cp -f /home/ec2-user/monitoring/prometheus.yml /opt/prometheus/prometheus.yml &>>$LOGFILE
-VALIDATE $? "Copying Prometheus configuration file"
+# Idempotent config copy
+if ! cmp -s /home/ec2-user/monitoring/prometheus.yml /opt/prometheus/prometheus.yml 2>/dev/null
+then
+    cp -f /home/ec2-user/monitoring/prometheus.yml /opt/prometheus/prometheus.yml &>>$LOGFILE
+    VALIDATE $? "Copying Prometheus configuration file"
+    NEEDS_RESTART=1
+else
+    echo -e "Prometheus configuration file unchanged....$Y SKIPPING $N" | tee -a $LOGFILE
+fi
 
-systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "Daemon reloading systemd"
+if [ "$NEEDS_RESTART" -eq 1 ]
+then
+    systemctl daemon-reload &>>$LOGFILE
+    VALIDATE $? "Daemon reloading systemd"
+else
+    echo -e "No config changes detected, skipping daemon-reload....$Y SKIPPING $N" | tee -a $LOGFILE
+fi
 
-systemctl start prometheus &>>$LOGFILE
-VALIDATE $? "Starting Prometheus service"
+# Idempotent start
+if systemctl is-active --quiet prometheus
+then
+    if [ "$NEEDS_RESTART" -eq 1 ]
+    then
+        systemctl restart prometheus &>>$LOGFILE
+        VALIDATE $? "Restarting Prometheus service (config changed)"
+    else
+        echo -e "Prometheus already running, no changes to apply....$Y SKIPPING $N" | tee -a $LOGFILE
+    fi
+else
+    systemctl start prometheus &>>$LOGFILE
+    VALIDATE $? "Starting Prometheus service"
+fi
 
-systemctl enable prometheus &>>$LOGFILE
-VALIDATE $? "Enabling Prometheus service"
-
-
-
+# Idempotent enable
+if systemctl is-enabled --quiet prometheus 2>/dev/null
+then
+    echo -e "Prometheus already enabled....$Y SKIPPING $N" | tee -a $LOGFILE
+else
+    systemctl enable prometheus &>>$LOGFILE
+    VALIDATE $? "Enabling Prometheus service"
+fi
