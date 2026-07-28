4f474dc0cc9a8410b828f097519c847ad8f68931
TGS_Manimohan <surya.manimohan@gmail.com>
2026-07-28 12:43:21 +0530

    updated frontend code

---
+++ frontend/main.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+set -e 
+failure(){
+    echo "ERROR on line $1:$2"
+}
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script as super user"
+    exit 1
+else
+    echo "Running the script as super user"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2....$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N"
+    fi
+}
+
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling Nginx service"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "Starting Nginx service"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "Removing default Nginx html files"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading frontend zip file"
+
+cd /usr/share/nginx/html &>>$LOGFILE
+VALIDATE $? "Changing directory to /usr/share/nginx/html"
+
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Extracting frontend zip file"
+
+cp -f /home/ec2-user/monitoring/frontend/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Copying expense.conf to /etc/nginx/default.d/"
+
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarting Nginx service"
